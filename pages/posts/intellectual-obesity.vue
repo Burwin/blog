ebca90350a79117655f3fa38011837bc72daea12
Michael Harris <michael@bamboo.dev>
2024-03-01 19:09:31 +0000

    fix single quotes

--- pages/posts/intellectual-obesity.vue
+++ pages/posts/intellectual-obesity.vue
@@ -22,7 +22,7 @@ import { bp, ba, bquote } from "../../components/elements";
     <bp> Eating a lot of food and not using it leads to obesity. </bp>
     <bp> Obesity is bad. </bp>
     <bquote
-      quote="Here’s something that’s even more cool: the food that’s stable across the entire domain of food isn’t food: it’s information. It’s information, and we use the same bloody circuits in our brain to forage for information that animals use to forage for food. It’s the same circuit. Why is that? Because we figured out that knowing where the food is is more important than having the food. Knowing where the food is is a form of meta-food—information is a form of meta food, and that’s why we’re information foragers. That idea is embedded into the story of Adam and Eve: whatever it is that they ingest is a form of meta food. It’s information."
+      quote="Here's something that's even more cool: the food that's stable across the entire domain of food isn't food: it's information. It's information, and we use the same bloody circuits in our brain to forage for information that animals use to forage for food. It's the same circuit. Why is that? Because we figured out that knowing where the food is is more important than having the food. Knowing where the food is is a form of meta-food—information is a form of meta food, and that's why we're information foragers. That idea is embedded into the story of Adam and Eve: whatever it is that they ingest is a form of meta food. It's information."
       name="Jordan Peterson"
       :context="{
         text: 'Biblical Series IV',
@@ -32,7 +32,7 @@ import { bp, ba, bquote } from "../../components/elements";
     </bquote>
     <bp>
       Like almost all software developers, especially the self-taught ones, I
-      love learning new things. It’s probably even a coping mechanism for
+      love learning new things. It's probably even a coping mechanism for
       stress… the trick being to at least channel it toward learning useful
       things.
     </bp>
