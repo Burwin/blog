6bfc4f9b8013c14120aef464e7e7c14678e8199b
Michael Harris <michael@bamboo.dev>
2024-03-01 19:49:20 +0000

    fix url for posts

--- analyze-all-posts.ts
+++ analyze-all-posts.ts
@@ -26,7 +26,7 @@ fs.readdir("./pages/posts", (err, files) => {
         date: new Date(dateMatch[1]),
         title: titleMatch[1],
         description: excerptMatch[1],
-        href: `/posts/${file.replace(/\.vue$/, "")}`,
+        href: `/blog/posts/${file.replace(/\.vue$/, "")}`,
         id: file.replace(/\.vue$/, ""),
       });
     }
