db769439ce3962262db7800caf0efe9d2f9d83cd
Michael Harris <michael@bamboo.dev>
2024-03-01 20:34:30 +0000

    add nav bar to blog posts

---
+++ components/BlogNavBar.vue
@@ -0,0 +1,52 @@
+<template>
+  <nav class="flex" aria-label="Breadcrumb">
+    <ol role="list" class="flex items-center space-x-4">
+      <li>
+        <div>
+          <a href="../../" class="text-gray-400 hover:text-gray-500">
+            <HomeIcon class="h-5 w-5 flex-shrink-0" aria-hidden="true" />
+            <span class="sr-only">Home</span>
+          </a>
+        </div>
+      </li>
+      <li v-for="page in pages" :key="page.name">
+        <div class="flex items-center">
+          <svg
+            class="h-5 w-5 flex-shrink-0 text-gray-300"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            aria-hidden="true"
+          >
+            <path d="M5.555 17.776l8-16 .894.448-8 16-.894-.448z" />
+          </svg>
+          <a
+            :href="page.href"
+            class="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700"
+            :aria-current="page.current ? 'page' : undefined"
+            >{{ page.name }}</a
+          >
+        </div>
+      </li>
+    </ol>
+  </nav>
+</template>
+
+<script setup lang="ts">
+import { HomeIcon } from "@heroicons/vue/20/solid";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+
+// home path is everything before /posts
+const pathTokens = route.path.split("/");
+const hometokens = pathTokens.slice(0, pathTokens.indexOf("posts") + 1);
+const homePath = hometokens.join("/");
+
+const pages = [
+  {
+    name: route.path.split("/").filter(Boolean).pop(),
+    href: route.path,
+    current: true,
+  },
+];
+</script>
--- components/BlogPost.vue
+++ components/BlogPost.vue
@@ -7,7 +7,10 @@ defineProps<{
 </script>
 
 <template>
-  <div class="bg-white px-6 py-32 lg:px-8">
+  <div class="bg-white pt-16 flex justify-center">
+    <BlogNavBar />
+  </div>
+  <div class="bg-white px-6 py-16 lg:px-8">
     <div class="mx-auto max-w-3xl text-base leading-7 text-gray-700">
       <main>
         <article>
