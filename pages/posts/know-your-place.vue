b16fd727d8516e71fb03caa81efb55464406330d
Michael Harris <michael@bamboo.dev>
2024-03-14 14:13:38 +0000

    know your place

---
+++ pages/posts/know-your-place.vue
@@ -0,0 +1,82 @@
+<script setup lang="ts">
+const post = {
+  date: "2024-03-14",
+  title: "Know Your Place",
+  excerpt: "... then fulfill it.",
+};
+</script>
+
+<template>
+  <BlogPost
+    :date="new Date(post.date)"
+    :title="post.title"
+    :excerpt="post.excerpt"
+  >
+    <ElementsBp>
+      Once a month, I get together with a small group of ambitious and brilliant
+      friends in Canton to eat steak and talk about business. The conversation
+      moves fast, and challenges me to keep up. I love it.
+    </ElementsBp>
+    <ElementsBp>
+      But I often leave with a feeling of angst, like I'm not doing enough in
+      business, or I'm letting opportunities slip by. They bring so many
+      orthogonal perspectives on business that I feel like I'm
+      <span class="italic">this close</span> to cracking the code and stumbling
+      upon a eureka moment.
+    </ElementsBp>
+    <ElementsBp>
+      We had one last night. Driving away, I had the same feeling. But this
+      time, I had my eureka:
+    </ElementsBp>
+    <ElementsBquote>
+      <ElementsBp>
+        Your obsession for the future is sabotaging your present, and your
+        future. Focus on optimizing where you are now, and the future will bloom
+        in time.
+      </ElementsBp>
+      <ElementsBp> Know your place. Fulfill it. </ElementsBp>
+    </ElementsBquote>
+    <ElementsBp>
+      It's not a new concept, but... it's <span class="italic">real</span> now.
+    </ElementsBp>
+    <ElementsBp>
+      A part of this was due to me realizing two things:
+    </ElementsBp>
+    <ElementsBul>
+      <ElementsBli>
+        I feel confident that if I just devoted enough time and energy, I could
+        start making a ton of money
+      </ElementsBli>
+      <ElementsBli>
+        But if I devoted this much time and energy to this, I seriously risk my
+        children turning out poorly, creating a dead end and making any wealth
+        I've gained completely pointless when I die
+      </ElementsBli>
+    </ElementsBul>
+    <ElementsBp>
+      So there's a weird dichotomy that somehow balances... In order to
+      meaningfully focus on my current situation, it helps to have a
+      multi-generational perspective. And when I primarily have a
+      multi-generational orientation, it naturally leads to me knowing that the
+      most important thing for me to do now is to focus on my current situation,
+      not worrying and planning so obsessively for the future.
+    </ElementsBp>
+    <ElementsBp> Know my place. </ElementsBp>
+    <ElementsBp>
+      My family is on the upswing, materially and spiritually. In order to
+      maintain that - sustainably - I need to approach family growth like any
+      other growth endeavor. If I went too hard in the gym, I would injure
+      myself and end up setting back progress instead of simply maintaining
+      humble consistency. If I stayed awake working until midnight 2-3 nights in
+      a row, I might get a marginal amount more accomplished, but would risk
+      getting sick, brain fog, burnout, etc.
+    </ElementsBp>
+    <ElementsBp>
+      I have ideas that I want to do in business, family, and community. Big
+      ideas. It's possible I'm the one that gets to implement some of them, but
+      it also just might not be my place. My first priority needs to be to
+      establish solid foundations so future generations can continue the path -
+      well-rested and without any slipped discs.
+    </ElementsBp>
+  </BlogPost>
+</template>
