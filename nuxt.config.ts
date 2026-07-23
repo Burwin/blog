e173713ba782c1ff86e2b1caf7d7d2e5caeb30a4
Michael Harris <michael@bamboo.dev>
2026-07-23 11:46:47 -0400

    clean up

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  compatibilityDate: '2026-07-23',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
   postcss: {
